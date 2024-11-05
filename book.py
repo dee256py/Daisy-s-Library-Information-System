4226dbc571a5f19eaa0243fb4887f0a19cd315cd
lillian7 <lnassanga@gmail.com>
2024-11-05 19:42:49 +0300

    Moved the book class to a new file

---
+++ book.py
@@ -0,0 +1,20 @@
+
+from deez_library import LibraryItem
+
+
+class Book(LibraryItem):
+    def __init__(self, title: str, author: str, year: int, genre: int):
+        super().__init__(title, author, year)
+        self.__genre = genre
+
+    #A function that allows a patron to borrow a book
+    def borrow(self):
+        if self.is_available():
+            self.mark_as_borrowed()
+            return f"You have borrowed the book: {self.get_details()}"
+        else:
+            return "This book is currently not available."
+
+    def return_item(self):
+        self.mark_as_available()
+        return f"You have returned the book: {self.get_details()}"
--- deez_library.py
+++ deez_library.py
@@ -22,24 +22,6 @@ class LibraryItem(ABC):
     
     #add getters and setter for all private attributes/properties
 
-#Class Book inherits Library Item
-class Book(LibraryItem):
-    def __init__(self, title: str, author: str, year: int, genre: int):
-        super().__init__(title, author, year)
-        self.__genre = genre
-
-    #A function that allows a patron to borrow a book
-    def borrow(self):
-        if self.is_available():
-            self.mark_as_borrowed()
-            return f"You have borrowed the book: {self.get_details()}"
-        else:
-            return "This book is currently not available."
-
-    def return_item(self):
-        self.mark_as_available()
-        return f"You have returned the book: {self.get_details()}"
-
 class Magazine(LibraryItem):
     def __init__(self, title, author, year, issue_number):
         super().__init__(title, author, year)
